1bdd4def637d55585c2aac08022585890a589fe0
Sebastian Rosengren <sebastian@rosengren.me>
2014-01-15 21:29:31 +0100

    init

---
+++ kotraining/Context/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using kotraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace kotraining.Context
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<School> Schools { get; set; }
+    }
+}
---
+++ kotraining/Models/School.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace kotraining.Models
+{
+    public class School
+    {
+        public int SchoolId { get; set; }
+        public string Name { get; set; }
+        public List<Class> Classes { get; set; }
+    }
+}
